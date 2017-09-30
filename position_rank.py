715caf945b8faaf9c3cab4354c014e5ac70b3587
ymym3412 <y.miura0916@gmail.com>
2017-09-30 20:25:38 +0900

    create position-rank and tokenizer script.

---
+++ position_rank.py
@@ -0,0 +1,120 @@
+# coding:utf-8
+import math
+import stemming.porter2 as porter
+import numpy as np
+import copy
+from collections import Counter
+
+
+def position_rank(sentence, tokenizer, alpha=0.85, window_size=6, num_keyphrase=10, lang="en"):
+    """Compute position rank score.
+
+    Position rank is a method for extracting keyphrase from sentence.
+    This method is allowed any language if you provide tokenizer
+    that tokenize language you wanna apply position rank.
+    In the oroginal paper, authors used 'title' and 'abstract' of scholarly documents.
+    Original paper is here: http://aclweb.org/anthology/P/P17/P17-1102.pdf
+
+    Args:
+      sentence: Text concatenated title and abstract.
+      tokenizer: Object that tokenize sentence.
+        Tokenizer must has tokenize(), tokenize() receive sentence and return token list
+        and phrase list. See tokenizer.py and class of tokenizer for detail.
+      alpha: Damping parameter. This allows 'teleport' operation into another node in the graph.
+      window_size: Size of woindow for co-occurece word.
+      num_keyphrase: Number of keyphrase which method will return.
+      lang: Target language.
+
+    Returns:
+      Keyphrase list. Length of list is decided by 'num_keyphrase' param.
+
+    """
+    if lang == "en":
+        stem = porter.stem
+    else:
+        stem = lambda word: word
+    # origial words(=no stemming) and phrase list
+    original_words, phrases = tokenizer.tokenize(sentence)
+    # stemmed words
+    stemmed_word = [stem(word) for word in original_words]
+    unique_word_list = set([word for word in stemmed_word])
+    n = len(unique_word_list)
+
+    adjancency_matrix = np.zeros((n, n))
+    word2idx = {w: i for i, w in enumerate(unique_word_list)}
+    p_vec = np.zeros(n)
+    # store co-occurence words
+    co_occ_dict = {w: [] for w in unique_word_list}
+
+    # 1. initialize  probability vector
+    for i, w in enumerate(stemmed_word):
+        # add position score
+        p_vec[word2idx[w]] += float(1 / (i+1))
+        for window_idx in range(1, math.ceil(window_size / 2)+1):
+            if i - window_idx >= 0:
+                co_list = co_occ_dict[w]
+                co_list.append(stemmed_word[i - window_idx])
+                co_occ_dict[w] = co_list
+
+            if i + window_idx < len(stemmed_word):
+                co_list = co_occ_dict[w]
+                co_list.append(stemmed_word[i + window_idx])
+                co_occ_dict[w] = co_list
+
+    # 2. create adjancency matrix from co-occurence word
+    for w, co_list in co_occ_dict.items():
+        cnt = Counter(co_list)
+        for co_word, freq in cnt.most_common():
+            adjancency_matrix[word2idx[w]][word2idx[co_word]] = freq
+
+    adjancency_matrix = adjancency_matrix / adjancency_matrix.sum(axis=1)
+    p_vec = p_vec / p_vec.sum()
+    # principal eigenvector s
+    s_vec = np.ones(n) / n
+
+    # threshold
+    lambda_val = 1.0
+    loop = 0
+    # compute final principal eigenvector
+    while lambda_val > 0.001:
+        next_s_vec = copy.deepcopy(s_vec)
+        for i, (p, s) in enumerate(zip(p_vec, s_vec)):
+            next_s = (1 - alpha) * p + alpha * (weight_total(adjancency_matrix, i, s_vec))
+            next_s_vec[i] = next_s
+        lambda_val = np.linalg.norm(next_s_vec - s_vec)
+        s_vec = next_s_vec
+        loop += 1
+        if loop > 100:
+            break
+
+    # score original words and phrases
+    word_with_score_list = [(word, s_vec[word2idx[stem(word)]]) for word in original_words]
+    for phrase in phrases:
+        total_score = sum([s_vec[word2idx[stem(word)]] for word in phrase.split("_")])
+        word_with_score_list.append((phrase, total_score))
+
+    sort_list = np.argsort([t[1] for t in word_with_score_list])
+    keyphrase_list = []
+    # if not check stemmed keyphrase, there are similar phrases in keyphrase list
+    # i.e. "neural network" and "neural networks" in list
+    stemmed_keyphrase_list = []
+    for idx in reversed(sort_list):
+        keyphrase = word_with_score_list[idx][0]
+        stemmed_keyphrase = " ".join([stem(word) for word in keyphrase.split("_")])
+        if not stemmed_keyphrase in stemmed_keyphrase_list:
+            keyphrase_list.append(keyphrase)
+            stemmed_keyphrase_list.append(stemmed_keyphrase)
+        if len(keyphrase_list) >= num_keyphrase:
+            break
+    return keyphrase_list
+
+
+def weight_total(matrix, idx, s_vec):
+    """Sum weights of adjacent nodes.
+
+    Choose 'j'th nodes which is adjacent to 'i'th node.
+    Sum weight in 'j'th column, then devide wij(weight of index i,j).
+    This calculation is applied to all adjacent node, and finally return sum of them.
+
+    """
+    return sum([(wij / matrix.sum(axis=0)[j]) * s_vec[j] for j, wij in enumerate(matrix[idx]) if not wij == 0])
---
+++ tokenizer.py
@@ -0,0 +1,122 @@
+from stanfordcorenlp import StanfordCoreNLP
+import re
+import MeCab
+
+
+class StanfordCoreNlpTokenizer(object):
+    """Tokenizer for English using Stanford CoreNLP for tokenization.
+
+    This class tokenize English sentence.
+    As a default, tokenizer returns tokens whhic POS are adjective or noun.
+    Simultaneously, tokenizer returns specific pattern phrases.
+
+    """
+    def __init__(self, url_or_path):
+        """Initialize stanford core nlp tokenier.
+
+        Args:
+          url_or_path: Url string of path string of Stanford CoreNLP library.
+            Provide url string if you already stand up Stanford CoreNLP server.
+            If not, provide path to directory of library i.e. JavaLibraries/stanford-corenlp-full-2017-06-09/.
+            When you provide path of librart, Stanford CoreNLP server will be up independent of python process.
+
+        """
+        self.tokenizer = StanfordCoreNLP(url_or_path)
+
+    def tokenize(self, sentence, pos_filter=["JJ", "JJR", "JJS", "NN", "NNS", "NNP", "NNPS"]):
+        """Tokenize sentence.
+
+        Tokenize sentence and return token list and phrase list.
+        Phrase is continuous tokens which have specific POS pattern '(adjective)*(noun)+'
+        and length are more than 3.
+        You can edit filter of token POS, default are limited to adjective and noun.
+
+        Args:
+          sentence: English sentence.
+          pos_filter: POS filter of token. Default are adjective and noun.
+
+        Returns:
+          Token list: Filterd by 'pos_filter' param.
+          Phrase list: Specific continuous tokens.
+
+        """
+        # (adj)*(noun)+で長さが3以上のフレーズも返す
+        tokens = self.tokenizer.pos_tag(sentence)
+        pos_tags = [self._anonymize_pos(token[1]) for token in tokens]
+        pattern = r"J*N+"
+        iterator = re.finditer(pattern, "".join(pos_tags))
+        phrases = filter(lambda x: len(x) >= 3, [[token[0] for token in tokens[match.start():match.end()]] for match in iterator])
+        phrases = ["_".join(phrase) for phrase in phrases]
+        return [token[0] for token in tokens if token[1] in pos_filter], phrases
+
+    def _anonymize_pos(self, pos):
+        """Anonymize POS tags.
+        Adjective tags are replaced to 'J', noun are to 'N', and others are to 'O'.
+
+        """
+        if (pos == "JJ") or (pos == "JJR") or (pos == "JJS"):
+            return "J"
+        elif (pos == "NN") or (pos == "NNS") or (pos == "NNP") or (pos == "NNPS"):
+            return "N"
+        else:
+            return "O"
+
+
+class MecabTokenizer(object):
+    """Tokenizer for Japanese using Mecab.
+
+    This class tokenize Japanese sentence.
+    As a default, tokenizer returns tokens whhic POS are adjective(形容詞) or noun(名詞).
+    Simultaneously, tokenizer returns specific pattern phrases.
+    This tokenizer requires that Mecab is installed.
+
+    """
+
+    def __init__(self, mecab_args="mecabrc"):
+        """Initialize tokenizer.
+
+        Args:
+          mecab_args: Argument of mecab.
+            i.e. '-Ochasen', '-d /usr/local/lib/mecab/dic/mecab-ipadic-neologd'
+
+        """
+        self.tokenizer = MeCab.Tagger(mecab_args)
+
+    def tokenize(self, sentence, pos_filter=["名詞", "形容詞"]):
+        """Tokenize sentence.
+
+        Tokenize sentence and return token list and phrase list.
+        Phrase is continuous tokens which have specific POS pattern '(adjective(形容詞))*(noun(名詞))+'
+        and length are more than 3.
+        You can edit filter of token POS, default are limited to adjective and noun.
+
+        Args:
+          sentence: Japanese sentence.
+          pos_filter: POS filter of token. Default are adjective and noun.
+
+        Returns:
+          Token list: Filterd by 'pos_filter' param.
+          Phrase list: Specific continuous tokens.
+
+        """
+        tokens = [(morph.split("\t")[0], morph.split("\t")[1].split(",")[0]) \
+                    for morph in self.tokenizer.parse(sentence).split("\n") \
+                        if not ((morph.split("\t")[0] == "EOS") or (morph.split("\t")[0] == ""))]
+        pos_tags = [self._anonymize_pos(token[1]) for token in tokens]
+        pattern = r"形*名+"
+        iterator = re.finditer(pattern, "".join(pos_tags))
+        phrases = filter(lambda x: len(x) >= 3, [[token[0] for token in tokens[match.start():match.end()]] for match in iterator])
+        phrases = ["_".join(phrase) for phrase in phrases]
+        return [token[0] for token in tokens if token[1] in pos_filter], phrases
+
+    def _anonymize_pos(self, pos):
+        """Anonymize POS tags.
+        Adjective tags are replaced to '形', noun are to '名', and others are to '他'.
+
+        """
+        if (pos == "形容詞"):
+            return "形"
+        elif (pos == "名詞"):
+            return "名"
+        else:
+            return "他"
